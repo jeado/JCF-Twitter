fa543e91906b7bb94b04479ac9ad64fb0f9f930f
Jeado <haibane84@gmail.com>
2012-02-12 02:08:32 +0900

    JCF-Twitter 과제 공유

---
+++ JCF-Twitter/src/main/java/jcf/edu/user/fileHandler/UserPicUploadEventHandler.java
@@ -0,0 +1,33 @@
+package jcf.edu.user.fileHandler;
+
+
+import javax.servlet.http.HttpServletRequest;
+
+import jcf.upload.FileInfo;
+import jcf.upload.MultiPartInfo;
+import jcf.upload.handler.UploadEventHandler;
+import jcf.upload.persistence.PersistenceManager;
+
+public class UserPicUploadEventHandler implements UploadEventHandler {
+
+	public long getMaxUploadSize() {
+		return 10000000;
+	}
+
+	public String getFolder(HttpServletRequest request) {
+		return "userpic";
+	}
+
+	public void postprocess(String folder, MultiPartInfo info,
+			PersistenceManager persistenceManager) {
+	}
+
+	public String createFileNameIfAccepted(String folder, FileInfo fileInfo) {
+		return fileInfo.getFieldName();
+	}
+
+	public void prepareStorage(PersistenceManager persistenceManager,
+			String folder) {
+	}
+
+}
